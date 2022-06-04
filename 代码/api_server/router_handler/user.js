845ec77ca0420047e2770209c369db6bbd8e1984
changfulhj <73992782+fieldpurse@users.noreply.github.com>
2022-06-04 17:59:08 +0800

    登录接口

--- 代码/api_server/router_handler/user.js
+++ 代码/api_server/router_handler/user.js
@@ -1,5 +1,7 @@
 const db = require('../db/index')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const config = require('../config');
 
 exports.regUser = (req, res) => {
   //判断为空的状态
@@ -7,7 +9,7 @@ exports.regUser = (req, res) => {
   if (!userinfo.username || !userinfo.password) {
     return res.cc('用户名或密码不能为空')
   }
-  if(userinfo.password.length < 6 || userinfo.password.length > 16) {
+  if (userinfo.password.length < 6 || userinfo.password.length > 16) {
     return res.cc('密码应在8到16位之间')
   }
   //判断是否占用用户名
@@ -27,11 +29,11 @@ exports.regUser = (req, res) => {
     //插入新用户
 
     const sql = 'insert into admins set ?'
-    db.query(sql, {username:userinfo.username, password:userinfo.password},(err, results) => {
+    db.query(sql, { username: userinfo.username, password: userinfo.password }, (err, results) => {
       //判断sql语句是否成功
-      if(err) return res.cc(err)
+      if (err) return res.cc(err)
       //判断影响行数是否为一
-      if(results.affectedRows !== 1) return res.send({state:'1', message:'注册用户失败，一会儿再试试八'})
+      if (results.affectedRows !== 1) return res.send({ state: '1', message: '注册用户失败，一会儿再试试八' })
       //成功的话
       res.cc('注册成功', 0)
     })
@@ -47,9 +49,32 @@ exports.logUser = (req, res) => {
     return res.cc('用户名或密码不能为空')
   }
 
-  const sql = 'select * from ';
+  const sql = 'select * from admins where username = ?';
 
+  db.query(sql, userinfo.username, (err, results) => {
+    //错误处理
+    if (err) return res.cc(err)
+    console.log(results.length)
+    if (results.length !== 1) res.cc('未知错误咯，安逸')
 
+    const compareResult = bcrypt.compareSync(userinfo.password, results[0].password)
+    
+    if(!compareResult) return res.cc('登录失败')
+
+    //登录成功后获取用户对象
+    const user = { ...results[0], password: '' }
+
+    //生成token
+    const tokenStr = jwt.sign(user, config.key, {
+      expiresIn: config.time, // token 有效期为 10 个小时
+    })
+
+    res.send({
+      state: 0,
+      message: '登录成功',
+      token: 'Bearer' + tokenStr
+    })
+  })
 
 
 
