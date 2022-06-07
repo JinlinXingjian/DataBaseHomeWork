1ea551600a89c604f19e29fcfb198dc3a506afc8
changfulhj <73992782+fieldpurse@users.noreply.github.com>
2022-06-07 20:06:03 +0800

    数据库编写(1)

--- 代码/api_server/router_handler/cars.js
+++ 代码/api_server/router_handler/cars.js
@@ -2,6 +2,7 @@ const { del } = require('express/lib/application')
 const db = require('../db/index')
 
 exports.look = (req, res) => {
+  
   const sql = 'select * from 车辆'
 
   db.query(sql, (err, results) => {
