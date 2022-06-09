310ae599c0e3bafdc03668d223c4747a026fd12a
changfulhj <73992782+fieldpurse@users.noreply.github.com>
2022-06-09 21:16:56 +0800

    小小规范化（细节）

--- code/api_server/router/cars.js
+++ code/api_server/router/cars.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+//引入处理函数
 const user_handler = require('../router_handler/cars')
 
+//路由定义
 router.get('/look', user_handler.look)
 
 router.post('/delete', user_handler.delete)
@@ -11,7 +13,5 @@ router.post('/update', user_handler.update)
 
 router.post('/add', user_handler.add)
 
-
-
-
+//导出路由
 module.exports = router
--- code/api_server/router_handler/log.js
+++ code/api_server/router_handler/log.js
@@ -1,10 +1,11 @@
+//引入数据库配置
 const db = require('../db/index')
 
+//出入记录查看处理函数
 exports.look = (req, res) => {
   const sql = "select * from 出入记录"
 
   db.query(sql, (err, results) => {
     res.send(results)
-
   })
 }
