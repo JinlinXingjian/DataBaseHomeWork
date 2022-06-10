6922d066538a8b3ffb1ff7a920acaae74a46a7fa
JinlinXingjian <2647600900@qq.com>
2022-06-10 09:26:46 +0800

    111

--- code/web_server/config.js
+++ code/web_server/config.js
@@ -1,7 +1,8 @@
 Window.myConfig={
-    //ip:"http://192.168.1.100"
+    // ip:"http://192.168.1.100"
     ip: "http://127.0.0.1"
     // ip:"http://192.168.1.119"
+	// ip:"http://192.168.129.25"
 }
 //获取cookie
 function getCookie(cname){
