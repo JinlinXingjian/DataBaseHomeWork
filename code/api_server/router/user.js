a7054835c525b42dde940e7d309b71abc0e1c790
changfulhj <73992782+fieldpurse@users.noreply.github.com>
2022-06-14 21:13:05 +0800

    要交的文档

--- code/api_server/router/user.js
+++ code/api_server/router/user.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-
+//引入处理函数
 const user_handler = require('../router_handler/user');
 
+//定义路由
 router.post('/reguser', user_handler.regUser)
 
 router.post('/login', user_handler.logUser)
 
-
-
+//导出路由
 module.exports = router
